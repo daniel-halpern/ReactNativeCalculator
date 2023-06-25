84868dca746b553136f9589b4755c54e8da42286
daniel-halpern <82973812+daniel-halpern@users.noreply.github.com>
2023-06-24 20:53:18 -0400

    Made math work and added sqrt button

--- App.js
+++ App.js
@@ -12,59 +12,52 @@ const NumButton = props => {
   )
 }
 
-const OperatorButton = props => {
-  const { term1, setTerm1, setAns, ans } = props;
-  return (
-    
-    <TouchableOpacity style={styles.button}
-      onPress={() => {
-      setTerm1(toString(ans))
-      setAns(0)
-      }}>
-      {/*Set Term 1 to the current answer displayed
-      Reset current display*/}
-      <Text style={styles.buttonText}>{props.name}</Text>
-  </TouchableOpacity>
-  )
+// Mess of code to try and make sure the answer fits on screen
+function round(num) {
+  numToString = num.toString()
+  if (num[0] == '-' && numToString.length > 9) {
+    if (numToString[9] == '.') {
+      return(numToString.slice(0,11))
+    } else {
+      return(numToString.slice(0,10))
+    }
+  } else if (numToString.length > 9) {
+    if (numToString[8] == '.') {
+      return(numToString.slice(0,10))
+    } else {
+      return(numToString.slice(0,9))
+    }
+  } else {
+    return(num)
+  }
 }
-
-const Button = props => {
-  return (
-    <TouchableOpacity style={styles.button}
-      onPress={() => numPress(props.name, props.term1)}>
-      <Text style={styles.buttonText}>{props.name}</Text>
-  </TouchableOpacity>
-  )
-}
-
-//function equalsPress(term1)
-
-//operatorPress
 
 function numPress(num, term) {
   term += num
   if (term[0] == '0') {
     term = term.substring(1)
   }
-  console.log(term)
-  return(term)
+  console.log(round(term))
+  return(round(term))
   
 }
 
 //clearPress
 export default function App() {
   const [ans, setAns] = useState("0");
-  const [term1, setTerm1] = useState(0);
+  const [term1, setTerm1] = useState("0");
+  const [term2, setTerm2] = useState("0")
   const [operator, setOperator] = useState(0);
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.answer}>{ans}</Text>
+      <Text style={styles.answer}>{round(ans)}</Text>
       <View style={styles.buttonContainer}>
 
         <TouchableOpacity style={styles.button}
           onPress={() => {
           setAns("0")
           setOperator(0)
+          setTerm1(0)
           //Clear other terms
           }}>
           <Text style={styles.buttonText}>c</Text>
@@ -85,54 +78,87 @@ export default function App() {
           onPress={() => {
           setAns((parseFloat(ans)/100).toString())
           console.log((parseFloat(ans)/100).toString())
-
-          //setAns(toString(parseFloat(ans)/100))
           }}>
           <Text style={styles.buttonText}>%</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[operator === 1 ? styles.buttonOn : styles.button]}
+        <TouchableOpacity style={styles.button}
+          onPress={() => {
+          console.log(Math.sqrt(parseFloat(ans)).toString())
+          setAns(Math.sqrt(parseFloat(ans)).toString())
+          }}>
+          <Text style={styles.buttonText}>√</Text>
+        </TouchableOpacity>
+
+        <NumButton name="7" setAns = {setAns} ans = {ans}/>
+        <NumButton name="8" setAns = {setAns} ans = {ans}/>
+        <NumButton name="9" setAns = {setAns} ans = {ans}/>
+
+        <TouchableOpacity style={[operator === 1 && ans == "0"  ? styles.buttonOn : styles.button]}
           onPress={() => {
           setOperator(1)
+          setTerm1(ans)
+          setAns("0")
           }}>
           <Text style={styles.buttonText}>÷</Text>
         </TouchableOpacity>  
 
-        <NumButton name="7" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="8" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="9" term1={term1} setAns = {setAns} ans = {ans}/>
+        <NumButton name="4" setAns = {setAns} ans = {ans}/>
+        <NumButton name="5" setAns = {setAns} ans = {ans}/>
+        <NumButton name="6" setAns = {setAns} ans = {ans}/>
 
-        <TouchableOpacity style={[operator === 2 ? styles.buttonOn : styles.button]}
+        <TouchableOpacity style={[operator === 2 && ans == "0" ? styles.buttonOn : styles.button]}
           onPress={() => {
           setOperator(2)
+          setTerm1(ans)
+          setAns("0")
           }}>
           <Text style={styles.buttonText}>x</Text>
         </TouchableOpacity>  
 
-        <NumButton name="4" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="5" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="6" term1={term1} setAns = {setAns} ans = {ans}/>
+        <NumButton name="1" setAns = {setAns} ans = {ans}/>
+        <NumButton name="2" setAns = {setAns} ans = {ans}/>
+        <NumButton name="3" setAns = {setAns} ans = {ans}/>
 
-        <TouchableOpacity style={[operator === 3 ? styles.buttonOn : styles.button]}
+        <TouchableOpacity style={[operator === 3  && ans == "0" ? styles.buttonOn : styles.button]}
           onPress={() => {
           setOperator(3)
+          setTerm1(ans)
+          setAns("0")
           }}>
           <Text style={styles.buttonText}>-</Text>
         </TouchableOpacity>  
 
-        <NumButton name="1" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="2" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="3" term1={term1} setAns = {setAns} ans = {ans}/>
-        <TouchableOpacity style={[operator === 4 ? styles.buttonOn : styles.button]}
+        <NumButton name="0" setAns = {setAns} ans = {ans}/>
+        <NumButton name="." setAns = {setAns} ans = {ans}/>
+
+        <TouchableOpacity style={styles.button}
+          onPress={() => {
+            if (operator == 1) {
+              answer = parseFloat(term1) / parseFloat(ans)
+            } else if (operator == 2) {
+              answer = parseFloat(term1) * parseFloat(ans)
+            } else if (operator == 3) {
+              answer = parseFloat(term1) - parseFloat(ans)
+            } else if (operator == 4) {
+              answer = parseFloat(term1) + parseFloat(ans)
+            }
+            answer.toString()
+            setTerm1("0")
+            setAns(answer)            
+          }}>
+          <Text style={styles.buttonText}>=</Text>
+        </TouchableOpacity>  
+
+        <TouchableOpacity style={[operator === 4 && ans == "0" ? styles.buttonOn : styles.button]}
           onPress={() => {
           setOperator(4)
+          setTerm1(ans)
+          setAns("0")
           }}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>  
-        <NumButton name="0" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="0" term1={term1} setAns = {setAns} ans = {ans}/>
-        <NumButton name="." term1={term1} setAns = {setAns} ans = {ans}/>
-        <Button name="="/>
+
       </View>
     </SafeAreaView>
   );
